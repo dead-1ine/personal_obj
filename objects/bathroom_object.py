d9fc42a151fcddac3dac804c002c80e211a23918
dead_line <yang991211@naver.com>
2023-04-22 22:28:32 +0900

    chore: Make Objects Directory  - Bathrooom Object      - Faucet

---
+++ objects/bathroom_object.py
@@ -0,0 +1,13 @@
+class Faucet:
+    def __init__(self):
+        self._on = False
+
+    def turn_on(self):
+        self._on = True
+
+    def turn_off(self):
+        self._on = False
+
+    def is_on(self):
+        return self.on
+
