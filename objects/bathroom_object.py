fd11a03ff2a1f8399d9e30fdb9c41fe626a15853
dead_line <yang991211@naver.com>
2023-04-26 23:14:01 +0900

    feat: Add new objects - Loofah - Towel

--- objects/bathroom_object.py
+++ objects/bathroom_object.py
@@ -11,3 +11,24 @@ class Faucet:
     def is_on(self):
         return self._on
 
+
+class Loofah:
+    def __init__(self):
+        self._is_soapy = False
+
+    def soap(self):
+        self._is_soapy = True
+
+    def clean(self):
+        self._is_soapy = False
+
+
+class Towel:
+    def __init__(self):
+        self._is_dry = True
+
+    def dry(self):
+        self._is_dry = True
+
+    def wet(self):
+        self._is_dry = False
